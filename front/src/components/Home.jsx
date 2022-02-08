2fd551c962194a0dddbeb6e425c20c6eccd01b23
Nishi <nishi05.bn@gmail.com>
2022-02-08 12:35:56 +0900

    add image & add error status

--- front/src/components/Home.jsx
+++ front/src/components/Home.jsx
@@ -1,7 +1,15 @@
 import React, { Component } from 'react'
-
+import Ticket from './../images/movie_tickets.jpg'
+import './Home.css'
 export default class Home extends Component {
   render() {
-    return <h2>Home</h2>
+    return (
+      <div className="text-center">
+        <h2>This is the home page</h2>
+        <hr />
+        <img src={Ticket} alt="movie ticket" />
+        <div className="tickets"></div>
+      </div>
+    )
   }
 }
