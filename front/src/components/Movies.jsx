076478e2e00d38e06956a55b23f6d465175d8528
Nishi <nishi05.bn@gmail.com>
2022-02-09 20:23:32 +0900

    fix front

--- front/src/components/Movies.jsx
+++ front/src/components/Movies.jsx
@@ -9,6 +9,7 @@ export default class Movies extends Component {
   }
 
   componentDidMount() {
+    console.log(process.env.REACT_APP_API_URL)
     fetch(`${process.env.REACT_APP_API_URL}/v1/movies`)
       .then((response) => {
         console.log('Status code is', response.status)
